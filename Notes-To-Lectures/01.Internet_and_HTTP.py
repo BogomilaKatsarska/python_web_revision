e08f3849e229b2ddcdae5367e72f6a3f8a8ab638
Bogomila <bkatsarska@abv.bg>
2023-04-05 15:01:27 +0300

    Initial commit

---
+++ Notes-To-Lectures/01.Internet_and_HTTP.py
@@ -0,0 +1,24 @@
+'''
+1:11
+1. Internet - vast network that connects billions of devices together all over the world
+- ISP - Internet Service Provider
+- Network - a group of two or more devices that can communicate
+- Web Client(our browser - Internet Explorer, Mozilla, FireFox..) sends Request to Web Server.
+  Web Server receives the Request and sends it to Technology (Our Application).
+  Technology connects to DB and returns requested resources.
+- Servers are the machines that provide services to other machines
+- Clients are the machines that are used to connect to those services
+- Network Protocol - set of rules and standards that allow communication between network devices
+- Packets - every message, file or stream of information sent over computer networks is broken
+  down into small chunks called packets. Contents/Origin/Destination
+- IP address (Internet Protocol Address)- All the devices on the Internet have IP Address. Each IP address is unique to each computer or
+  a device at the edge of the network
+- DNS - The domain name is a human way to access IP address for devices and websites around the world (example: softuni.bg)
+
+2. HTTP (Hyper Text Transfer Protocol)
+- Methods:
+POST - create / store a resource
+GET - read / retrieve a resource
+PUT - update / modify a resource
+DELETE - delete / remove a resource
+'''
