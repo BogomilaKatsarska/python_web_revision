d1795a202e9ed69ff0767e3118d92f8ac56a2770
Bogomila <bkatsarska@abv.bg>
2023-04-18 17:10:53 +0300

    sixth commit - URLs and Views

--- Notes-To-Lectures/04.URLs_and_Views.py
+++ Notes-To-Lectures/04.URLs_and_Views.py
@@ -1,7 +1,11 @@
 '''
-1:14
 1. Slugs - unique
 2. Dynamic URLs
     path('department/<int:department_id>/', views.show_department_by_id)
     include converter type (e.g.: int:)
+3. Django Shortcut Functions (from django.shortcuts import render, redirect..)
+- render(request, template_name='departments/departments_by_id'
+4. Views returning errors
+5. Environmental Variables:
+SECRET_KEY = os.environ.get('SECRET_KEY', 'purple unicorn')
 '''
--- python_web_revision/departments/urls.py
+++ python_web_revision/departments/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
 
-from python_web_revision.departments.views import show_departments
+from python_web_revision.departments.views import show_departments, redirect_to_first_department, show_not_found
 
 urlpatterns = (
-    path('<int:department_id>/', show_departments),
+    path('<int:department_id>/', show_departments, name='show department by id'),
+    path('redirect/', redirect_to_first_department, name='redirect demo'),
+    path('not-found/', show_not_found, name='not found'),
 )
