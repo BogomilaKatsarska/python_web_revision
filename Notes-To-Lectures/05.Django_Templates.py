55c7426ecc4ec88f791d566aff7415a8777b9612
Bogomila <bkatsarska@abv.bg>
2023-05-02 11:07:14 +0300

    seventh commit - Templates

--- Notes-To-Lectures/05.Django_Templates.py
+++ Notes-To-Lectures/05.Django_Templates.py
@@ -1,3 +1,10 @@
 '''
-
+0:15
+SSR = Server-side rendering (HTTP response returns the needed template)
+CSR = Client-side rendering (React, Angular, Vue..)
+DTL and Jinja2
+1.Variables
+2.Filters
+3.Tags
+4.Commentsd
 '''
