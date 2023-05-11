fe560f0e6558da2a17cd181ece23fc13d429322e
Bogomila <bkatsarska@abv.bg>
2023-05-11 17:02:56 +0300

    11th commit - Forms part II

--- Notes-To-Lectures/09.Django_Forms_Part_Two.py
+++ Notes-To-Lectures/09.Django_Forms_Part_Two.py
@@ -1,3 +1,40 @@
 '''
-16:00
+- We use tuple of validators for forms:
+
+class TodoForm(forms.Form):
+    text = forms.CharField(
+        validators = (
+            validate_text,
+        ),
+    )
+    is_done = forms.BooleanField(
+        required = False,
+    )
+
+def clean(self):
+    return super().clean()
+
+def clean_text():
+    use for:
+    1. Transform data into desired format/form
+    2. Validation
+
+def clean_priority():
+    pass
+
+def clean_is_done():
+    pass
+
+
+if form.is_valid():
+    model = form.instance
+    model.full_clean() --> method of models!
+
+def clean_assignee(self):
+    assignee = self.cleaned_data['assignee']
+    if assignee.todo_set.count() >= 3:
+        raise ValidationError(f'{assignee} already has max todos assigned.')
+    return assignee
+
+
 '''
